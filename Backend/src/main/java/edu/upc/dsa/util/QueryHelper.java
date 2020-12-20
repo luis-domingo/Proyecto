e605dadba14a3dc59032ce362e70e3e72c9e375f
luis-domingo <72356764+luis-domingo@users.noreply.github.com>
2020-12-20 16:57:55 +0100

    Mas loggers

--- Backend/src/main/java/edu/upc/dsa/util/QueryHelper.java
+++ Backend/src/main/java/edu/upc/dsa/util/QueryHelper.java
@@ -10,26 +10,27 @@ public class QueryHelper {
     public static String createQueryINSERT(Object entity) {
 
         //INSERT INTO [table name] (Column, Column) VALUES (?,?);
-
+        logger.info("Preparando la sentencia");
         StringBuffer sb = new StringBuffer("INSERT INTO ");
         sb.append(entity.getClass().getSimpleName()).append(" ");
         sb.append("(");
         //INSERT INTO Usuario (
-
+        logger.info(sb.toString());
         String [] fields = ObjectHelper.getFields(entity);
         for (int i=1; i< fields.length; i++) {
             sb.append(fields[i]);
             sb.append(", ");
         }
         sb.append(fields[fields.length]);
-
+        logger.info(sb.toString());
         sb.append(") VALUES (?");
+        logger.info(sb.toString());
         //INSERT INTO Usuario (id, nombre, password) VALUES (?
 
         for (int i=1; i< fields.length; i++) {
             sb.append(", ?");
         }
-
+        logger.info(sb.toString());
         sb.append(")");
         //INSERT INTO Usuario (id, nombre, password) VALUES (?, ?, ?)
         logger.info(sb.toString());

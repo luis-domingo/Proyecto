244f0877036f2bf8eb865d480b4b4ba1a1cfc42f
pcc1999 <pcasana@protonmail.com>
2020-12-20 22:20:49 +0100

    Update SessionImpl.java

--- Backend/src/main/java/edu/upc/dsa/SessionImpl.java
+++ Backend/src/main/java/edu/upc/dsa/SessionImpl.java
@@ -29,7 +29,7 @@ public class SessionImpl implements Session {
             pstm = conn.prepareStatement(insertQuery);
             logger.info(pstm.toString());
 
-            int i = 1;
+            int i = 2;
             for(String field : ObjectHelper.getFields(entity)){
                 pstm.setObject(i,ObjectHelper.getter(entity, field));
                 i++;

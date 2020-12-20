93f2f087e2427b77cf8e0047e1745b22e4ffd7c0
luis-domingo <72356764+luis-domingo@users.noreply.github.com>
2020-12-20 12:24:19 +0100

    CredencialesBuenas

--- Backend/src/main/java/edu/upc/dsa/FactorySession.java
+++ Backend/src/main/java/edu/upc/dsa/FactorySession.java
@@ -36,8 +36,7 @@ public class FactorySession {
         Connection conn = null;
         try {
             conn =
-                    DriverManager.getConnection("jdbc:mariadb://"+host+":"+port+"/"+db);
-                            //"?user="+user+"&password="+pass);
+                    DriverManager.getConnection("jdbc:mariadb://"+host+":"+port+"/"+db+"?user="+user+"&password="+pass);
 
         } catch (SQLException ex) {
             // handle any errors

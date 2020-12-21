cb2cec06d1b8264c07eaf66893e1ebe39956319f
pcc1999 <pcasana@protonmail.com>
2020-12-21 01:54:17 +0100

    Login using database

--- Backend/src/main/java/edu/upc/dsa/models/Usuario.java
+++ Backend/src/main/java/edu/upc/dsa/models/Usuario.java
@@ -40,4 +40,13 @@ public class Usuario {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }

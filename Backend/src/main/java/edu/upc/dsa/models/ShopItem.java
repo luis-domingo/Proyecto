091905a144990479512a49c1e1fe8570f326f0e7
pcc1999 <pcasana@protonmail.com>
2021-01-07 01:51:14 +0100

    Updated models

--- Backend/src/main/java/edu/upc/dsa/models/ShopItem.java
+++ Backend/src/main/java/edu/upc/dsa/models/ShopItem.java
@@ -6,4 +6,28 @@ public class ShopItem {
     String name;
     int price;
     String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
--- Backend/src/main/java/edu/upc/dsa/models/UserItem.java
+++ Backend/src/main/java/edu/upc/dsa/models/UserItem.java
@@ -6,4 +6,28 @@ public class UserItem {
     String name;
     int quantity;
     String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }

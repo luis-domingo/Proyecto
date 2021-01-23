5413a4c468b75bb8cc142e0de365e57ff4ec47db
pcc1999 <pcasana@protonmail.com>
2021-01-23 20:24:26 +0100

    Minor fixes

--- Backend/src/main/java/edu/upc/dsa/models/UserItem.java
+++ Backend/src/main/java/edu/upc/dsa/models/UserItem.java
@@ -1,7 +1,5 @@
 package edu.upc.dsa.models;
 
-import edu.upc.dsa.models.Item;
-
 public class UserItem {
     String name;
     int quantity;
--- Backend/src/main/java/edu/upc/dsa/util/ObjectHelper.java
+++ Backend/src/main/java/edu/upc/dsa/util/ObjectHelper.java
@@ -25,7 +25,6 @@ public class ObjectHelper {
 
     }
 
-
     public static void setter(Object object, String property, Object value) {
         //Cogemos el nombre de la clase del objeto
         String className = object.getClass().getName();
@@ -33,18 +32,13 @@ public class ObjectHelper {
         String method = "set" + property.substring(0, 1).toUpperCase() + property.substring(1);
         //Cogemos la clase
         Class classType = value.getClass();
-        Field[] fields = object.getClass().getFields();
-        for (Field field : fields) {
-            if (field.toString().equals(property)) {
-                try {
-                    //Creamos el setter
-                    Method setter = Class.forName(className).getDeclaredMethod(method, classType);
-                    //Lo invocamos
-                    setter.invoke(object, value);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+        try {
+            //Creamos el setter
+            Method setter = Class.forName(className).getDeclaredMethod(method, classType);
+            //Lo invocamos
+            setter.invoke(object, value);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 

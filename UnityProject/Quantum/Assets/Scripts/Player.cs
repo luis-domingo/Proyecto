5baa44bd6df0692b724577f6b23c7b35c534bd54
Skry777 <axel.rogui@gmail.com>
2021-01-24 18:36:58 +0100

    flechitas

--- UnityProject/Quantum/Assets/Scripts/Player.cs
+++ UnityProject/Quantum/Assets/Scripts/Player.cs
@@ -9,10 +9,64 @@ public class Player : MonoBehaviour
 
 	public float speed=3;
 	Rigidbody2D rb;
+    
+    //definimos los objetos
+
+    public int pointsCrystals = 10;
+    public int pointsPills = 20;
+    public int pointsDamage = 5;
+    //public Text CrystalsText;
+    //public Text HealthText;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    public int Crystals = 0;
+    public int Health  = 70;
+    public bool Derbool, Izqbool, Upbool, Downbool;
+
+    public void DerTrue()
+    {
+        Derbool = true;
+    }
+    public void Derfalse()
+    {
+        Derbool = false;
+    }
+    public void IzqTrue()
+    {
+        Izqbool = true;
+    }
+    public void Izqfalse()
+    {
+        Izqbool = false;
+    }
+    public void UpTrue()
+    {
+        Upbool = true;
+    }
+    public void Upfalse()
+    {
+        Upbool = false;
+    }
+    public void DownTrue()
+    {
+        Downbool = true;
+    }
+    public void Downfalse()
+    {
+        Downbool = false;
+    }
 	// Use this for initialization
 	private void Start()
 	{
 		rb= GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Crystals = GameManager.instance.playerCrystals;
+        Health = GameManager.instance.playerHealth;
+        //CrystalText.text = "Crystal: " + Crystals;
+        //HealthText.text = "Health: " + Health;
 	}
 
 
@@ -22,7 +76,7 @@ public class Player : MonoBehaviour
 	{
 
 
-		if (Input.GetKey(KeyCode.D)){
+		if (Input.GetKey(KeyCode.D) || Derbool){
 			if (GetComponent<SpriteRenderer>().flipX==true){
 				GetComponent<SpriteRenderer>().flipX=false;
 			}
@@ -31,7 +85,7 @@ public class Player : MonoBehaviour
 			rb.velocity=new Vector2(speed,0);
 
 		}
-		else if (Input.GetKey(KeyCode.A)){
+		else if (Input.GetKey(KeyCode.A) || Izqbool){
 			if (GetComponent<SpriteRenderer>().flipX==false){
 				GetComponent<SpriteRenderer>().flipX=true;
 			}
@@ -39,7 +93,7 @@ public class Player : MonoBehaviour
 			//transform.Translate(-0.015f,0,0);
 			rb.velocity=new Vector2(-speed,0);
 		}
-		else if (Input.GetKey(KeyCode.W)){
+		else if (Input.GetKey(KeyCode.W) || Upbool){
 			if (GetComponent<SpriteRenderer>().flipY==true){
 				GetComponent<SpriteRenderer>().flipY=false;
 			}
@@ -47,21 +101,23 @@ public class Player : MonoBehaviour
 			//transform.Translate(0,0.015f,0);
 			rb.velocity=new Vector2(0,speed);
 		}
-		else if (Input.GetKey(KeyCode.S)){
+		else if (Input.GetKey(KeyCode.S) || Downbool){
 
 			GetComponent<Animator>().SetBool("Walk",true);
 			//transform.Translate(0,-0.015f,0);
 			rb.velocity=new Vector2(0,-speed);
 		}
 
-		if (Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.W)){
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W) ||(!Downbool && !Izqbool && !Derbool && !Upbool))
+        {
 			GetComponent<Animator>().SetBool("Walk",false);
 			rb.velocity= new Vector2(0,0);
 
 		}
 
-
+        
 	}
+
 
 
 

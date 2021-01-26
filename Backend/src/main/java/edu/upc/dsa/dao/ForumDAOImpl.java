ac31cb94ea4c2312569589dee7f49f57f889db16
pcc1999 <pcasana@protonmail.com>
2021-01-26 14:25:27 +0100

    Update ForumDAOImpl.java

--- Backend/src/main/java/edu/upc/dsa/dao/ForumDAOImpl.java
+++ Backend/src/main/java/edu/upc/dsa/dao/ForumDAOImpl.java
@@ -82,6 +82,7 @@ public class ForumDAOImpl implements ForumDAO{
             res = (ResultSet)session.get(tp);
             int updateNum = res.getInt(3);
             updateNum ++;
+            logger.info("El nuevo numero de publicaciones sera " + updateNum);
             tp.setNumPublications(String.valueOf(updateNum));
             session.save(tp);
         }

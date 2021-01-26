2237b25748114331e0783849891504d2a4ad7e51
Skry777 <axel.rogui@gmail.com>
2021-01-26 21:50:54 +0100

    Revert "Revert "integracion v1""
    This reverts commit 2f74c1fd8c13707915e400119f705fbc7d596b4a.

--- UnityProject/Quantum/Assets/Scripts/Player.cs
+++ UnityProject/Quantum/Assets/Scripts/Player.cs
@@ -14,11 +14,11 @@ public class Player : MonoBehaviour
 	private Text Cristals;
 	private Text Health;
 	private Text Pista;
-	private Text Time;
+    //private Text Time;
 	public float speed=3;
 	private int Crystals;
 	private int Healthnum;
-	private float Times;
+    //private float Times;
 	Rigidbody2D rb;
 	public AudioClip pillEfx;
 	public AudioClip crystalEfx;
@@ -90,11 +90,11 @@ public class Player : MonoBehaviour
 
 		Crystals = GameManager.instance.playerCrystals;
 		Healthnum= GameManager.instance.playerHealth;
-		Times = GameManager.instance.availableTime;
+        //Times = GameManager.instance.availableTime;
 		Cristals = GameObject.FindWithTag("cristalt").GetComponent(typeof(Text)) as Text;
 		Health = GameObject.FindWithTag("healtht").GetComponent(typeof(Text)) as Text;
 		Pista = GameObject.FindWithTag("pistat").GetComponent(typeof(Text)) as Text;
-		Time = GameObject.FindWithTag("timett").GetComponent(typeof(Text)) as Text;
+        //Time = GameObject.FindWithTag("timett").GetComponent(typeof(Text)) as Text;
 		pistaImage = GameObject.Find("PistaImage");
 		pistaImage.SetActive(false);
 		lvl = GameManager.instance.level;
@@ -106,7 +106,7 @@ public class Player : MonoBehaviour
 	{
 		GameManager.instance.playerHealth = Healthnum;
 		GameManager.instance.playerCrystals = Crystals;
-		GameManager.instance.availableTime = Times;
+        //GameManager.instance.availableTime = Times;
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
@@ -192,7 +192,7 @@ public class Player : MonoBehaviour
 	void Update()
 	{
 	    SomthingTrue();
-		CheckIfGameOverTime();
+        //CheckIfGameOverTime();
 
 		if (Input.GetKey(KeyCode.D) || Derbool){
 			if (GetComponent<SpriteRenderer>().flipX==true){
@@ -242,14 +242,14 @@ public class Player : MonoBehaviour
 		}
 	}
 
-	private void CheckIfGameOverTime()
-	{
-		if(Times <= 0 ) {
-			SoundManager.instance.RandomizeSfx(gameOverEfx);
-			SoundManager.instance.musicSource.Stop();
-			GameManager.instance.GameOver();
-		}
-	}
+    //private void CheckIfGameOverTime()
+    //{
+    //    if(Times <= 0 ) {
+    //        SoundManager.instance.RandomizeSfx(gameOverEfx);
+    //        SoundManager.instance.musicSource.Stop();
+    //        GameManager.instance.GameOver();
+    //    }
+    //}
 
 
 	private void Restart() {

c77cff1f1527c187e6db4b32449b178fe282c852
pcc1999 <pcasana@protonmail.com>
2021-01-26 16:45:37 +0100

    Update RandomUtils.java

--- Backend/src/main/java/edu/upc/dsa/util/RandomUtils.java
+++ Backend/src/main/java/edu/upc/dsa/util/RandomUtils.java
@@ -7,7 +7,7 @@ public class RandomUtils {
 
     public static String getId() {
         RandomStringGenerator generator = new RandomStringGenerator();
-        String randomString = generator.generateByRegex("\\w+\\d*[0-9]{0,8}");
+        String randomString = generator.generateByRegex("\\d*[0-9]{0,8}");
 
         return randomString;
     }
